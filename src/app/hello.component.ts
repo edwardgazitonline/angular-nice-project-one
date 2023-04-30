dcba055668142cdfa510b0e5c400fe3a87082b23
edwardgazitonline <75829088+edwardgazitonline@users.noreply.github.com>
2023-04-30 09:33:31 +0300

    3 files modified

--- src/app/hello.component.ts
+++ src/app/hello.component.ts
@@ -2,7 +2,8 @@ import { Component, Input } from '@angular/core';
 
 @Component({
   selector: 'hello',
-  template: `<h1>Hello {{name}}!</h1>`,
+  template: `<h1>Hello</h1>
+  <h1> This is a Routing Test {{name}}!</h1>`,
   styles: [`h1 { font-family: Lato; }`]
 })
 export class HelloComponent  {
